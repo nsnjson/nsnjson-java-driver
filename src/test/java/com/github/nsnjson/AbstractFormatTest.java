dbded094d598a9e5454aa701dec31e612e3df53a
Maks Atygaev <jxcoder@ya.ru>
2015-10-28 15:13:30 +0700

    Implement AbstractFormatTest (data sets provider)

---
+++ src/test/java/com/github/nsnjson/AbstractFormatTest.java
@@ -0,0 +1,41 @@
+package com.github.nsnjson;
+
+import com.fasterxml.jackson.databind.node.*;
+
+import java.util.*;
+
+public class AbstractFormatTest {
+
+    protected static NullNode getNull() {
+        return NullNode.getInstance();
+    }
+
+    protected static BooleanNode getBooleanTrue() {
+        return BooleanNode.getTrue();
+    }
+
+    protected static BooleanNode getBooleanFalse() {
+        return BooleanNode.getFalse();
+    }
+
+    protected static NumericNode getNumberInt() {
+        return new IntNode(new Random().nextInt());
+    }
+
+    protected static NumericNode getNumberLong() {
+        return new LongNode(new Random().nextLong());
+    }
+
+    protected static NumericNode getNumberDouble() {
+        return new DoubleNode(new Random().nextDouble());
+    }
+
+    protected static TextNode getEmptyString() {
+        return new TextNode("");
+    }
+
+    protected static TextNode getString() {
+        return new TextNode(UUID.randomUUID().toString().replaceAll("-", ""));
+    }
+
+}
