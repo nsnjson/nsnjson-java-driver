3cf843b36b3eb62018d30232bb4da34b61f9eaf2
Maks Atygaev <jxcoder@ya.ru>
2015-10-28 23:04:52 +0700

    Implement method { AbstractFormatTest @ getObject }

--- src/test/java/com/github/nsnjson/AbstractFormatTest.java
+++ src/test/java/com/github/nsnjson/AbstractFormatTest.java
@@ -60,4 +60,17 @@ public class AbstractFormatTest {
         return new ObjectMapper().createObjectNode();
     }
 
+    protected static ObjectNode getObject() {
+        ObjectNode object = new ObjectMapper().createObjectNode();
+        object.set("null_field", getNull());
+        object.set("true_field", getBooleanTrue());
+        object.set("false_field", getBooleanFalse());
+        object.set("int_field", getNumberInt());
+        object.set("long_field", getNumberLong());
+        object.set("double_field", getNumberDouble());
+        object.set("string_field", getString());
+
+        return object;
+    }
+
 }
