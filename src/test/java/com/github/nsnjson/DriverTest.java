c60b8aa2e98993f871164ef080a7767d9bc53589
Maks Atygaev <jxcoder@ya.ru>
2015-10-31 06:51:55 +0700

    Refactor method { DriverTest @ processObject }

--- src/test/java/com/github/nsnjson/DriverTest.java
+++ src/test/java/com/github/nsnjson/DriverTest.java
@@ -142,91 +142,9 @@ public class DriverTest extends AbstractFormatTest {
 
     @Test
     public void processObject() {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode object = getObject();
 
-        String nullField = "null_field";
-
-        String numberIntField = "int_field";
-
-        String numberLongField = "long_field";
-
-        String numberDoubleField = "double_field";
-
-        String stringField = "string_field";
-
-        String booleanTrueField = "true_field";
-
-        String booleanFalseField = "false_field";
-
-        JsonNode numberInt = getNumberInt();
-
-        JsonNode numberLong = getNumberLong();
-
-        JsonNode numberDouble = getNumberDouble();
-
-        JsonNode string = getString();
-
-        JsonNode booleanTrue = getBooleanTrue();
-
-        JsonNode booleanFalse = getBooleanFalse();
-
-        ObjectNode object = objectMapper.createObjectNode();
-        object.set(nullField, getNull());
-        object.set(numberIntField, numberInt);
-        object.set(numberLongField, numberLong);
-        object.set(numberDoubleField, numberDouble);
-        object.set(stringField, string);
-        object.set(booleanTrueField, booleanTrue);
-        object.set(booleanFalseField, booleanFalse);
-
-        ObjectNode presentationOfNullField = objectMapper.createObjectNode();
-        presentationOfNullField.put(FIELD_NAME, nullField);
-        presentationOfNullField.put(FIELD_TYPE, TYPE_MARKER_NULL);
-
-        ObjectNode presentationOfNumberIntField = objectMapper.createObjectNode();
-        presentationOfNumberIntField.put(FIELD_NAME, numberIntField);
-        presentationOfNumberIntField.put(FIELD_TYPE, TYPE_MARKER_NUMBER);
-        presentationOfNumberIntField.put(FIELD_VALUE, numberInt.asInt());
-
-        ObjectNode presentationOfNumberLongField = objectMapper.createObjectNode();
-        presentationOfNumberLongField.put(FIELD_NAME, numberLongField);
-        presentationOfNumberLongField.put(FIELD_TYPE, TYPE_MARKER_NUMBER);
-        presentationOfNumberLongField.put(FIELD_VALUE, numberLong.asLong());
-
-        ObjectNode presentationOfNumberDoubleField = objectMapper.createObjectNode();
-        presentationOfNumberDoubleField.put(FIELD_NAME, numberDoubleField);
-        presentationOfNumberDoubleField.put(FIELD_TYPE, TYPE_MARKER_NUMBER);
-        presentationOfNumberDoubleField.put(FIELD_VALUE, numberDouble.asDouble());
-
-        ObjectNode presentationOfStringField = objectMapper.createObjectNode();
-        presentationOfStringField.put(FIELD_NAME, stringField);
-        presentationOfStringField.put(FIELD_TYPE, TYPE_MARKER_STRING);
-        presentationOfStringField.put(FIELD_VALUE, string.asText());
-
-        ObjectNode presentationOfBooleanTrueField = objectMapper.createObjectNode();
-        presentationOfBooleanTrueField.put(FIELD_NAME, booleanTrueField);
-        presentationOfBooleanTrueField.put(FIELD_TYPE, TYPE_MARKER_BOOLEAN);
-        presentationOfBooleanTrueField.put(FIELD_VALUE, BOOLEAN_TRUE);
-
-        ObjectNode presentationOfBooleanFalseField = objectMapper.createObjectNode();
-        presentationOfBooleanFalseField.put(FIELD_NAME, booleanFalseField);
-        presentationOfBooleanFalseField.put(FIELD_TYPE, TYPE_MARKER_BOOLEAN);
-        presentationOfBooleanFalseField.put(FIELD_VALUE, BOOLEAN_FALSE);
-
-        ArrayNode presentationOfArrayItems = objectMapper.createArrayNode();
-        presentationOfArrayItems.add(presentationOfNullField);
-        presentationOfArrayItems.add(presentationOfNumberIntField);
-        presentationOfArrayItems.add(presentationOfNumberLongField);
-        presentationOfArrayItems.add(presentationOfNumberDoubleField);
-        presentationOfArrayItems.add(presentationOfStringField);
-        presentationOfArrayItems.add(presentationOfBooleanTrueField);
-        presentationOfArrayItems.add(presentationOfBooleanFalseField);
-
-        ObjectNode presentation = objectMapper.createObjectNode();
-        presentation.put(FIELD_TYPE, TYPE_MARKER_OBJECT);
-        presentation.set(FIELD_VALUE, presentationOfArrayItems);
-
-        shouldBeConsistencyWhenGivenObject(object, presentation);
+        shouldBeConsistencyWhenGivenObject(object, getObjectPresentation(object));
     }
 
     private void shouldBeConsistencyWhenGivenNull(NullNode value, ObjectNode presentation) {
