66673e6a66f5ef1b21844a38c9e80e5cedbf6963
Maks Atygaev <jxcoder@ya.ru>
2015-11-01 18:28:45 +0700

    Add constant { CustomFormat @ INDEX_NAME }

--- src/test/java/com/github/nsnjson/CustomFormat.java
+++ src/test/java/com/github/nsnjson/CustomFormat.java
@@ -6,6 +6,8 @@ public final class CustomFormat {
 
     public static final int INDEX_VALUE = 1;
 
+    public static final int INDEX_NAME = 0;
+
     public static final String TYPE_NAME_NULL = "null";
 
     public static final String TYPE_NAME_NUMBER = "number";
