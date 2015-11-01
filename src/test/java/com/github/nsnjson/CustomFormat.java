96c55b321ab464170123ac555b265e26e40d7f4a
Maks Atygaev <jxcoder@ya.ru>
2015-11-01 16:16:57 +0700

    Implement CustomFormat: format constants like type names

---
+++ src/test/java/com/github/nsnjson/CustomFormat.java
@@ -0,0 +1,17 @@
+package com.github.nsnjson;
+
+public final class CustomFormat {
+
+    public static final String TYPE_NAME_NULL = "null";
+
+    public static final String TYPE_NAME_NUMBER = "number";
+
+    public static final String TYPE_NAME_STRING = "string";
+
+    public static final String TYPE_NAME_BOOLEAN = "boolean";
+
+    public static final String TYPE_NAME_ARRAY = "array";
+
+    public static final String TYPE_NAME_OBJECT = "object";
+
+}
