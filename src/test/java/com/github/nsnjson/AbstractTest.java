c8bbf6278124e631767180ca30c4de2a5e53ebd2
Maks Atygaev <jxcoder@ya.ru>
2015-11-01 16:26:51 +0700

    Pull up test methods to AbstractTest

--- src/test/java/com/github/nsnjson/AbstractTest.java
+++ src/test/java/com/github/nsnjson/AbstractTest.java
@@ -2,11 +2,109 @@ package com.github.nsnjson;
 
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.node.*;
-import org.junit.Assert;
+import org.junit.*;
 
 import java.util.*;
 
 public abstract class AbstractTest {
+
+    @Test
+    public void testNull() {
+        processTestNull(getNull(), getNullPresentation());
+    }
+
+    @Test
+    public void testNumberInt() {
+        NumericNode value = getNumberInt();
+
+        processTestNumberInt(value, getNumberIntPresentation(value));
+    }
+
+    @Test
+    public void testNumberLong() {
+        NumericNode value = getNumberLong();
+
+        processTestNumberLong(value, getNumberLongPresentation(value));
+    }
+
+    @Test
+    public void testNumberDouble() {
+        NumericNode value = getNumberDouble();
+
+        processTestNumberDouble(value, getNumberDoublePresentation(value));
+    }
+
+    @Test
+    public void testEmptyString() {
+        TextNode value = getEmptyString();
+
+        processTestString(value, getStringPresentation(value));
+    }
+
+    @Test
+    public void testString() {
+        TextNode value = getString();
+
+        processTestString(value, getStringPresentation(value));
+    }
+
+    @Test
+    public void testBooleanTrue() {
+        BooleanNode value = getBooleanTrue();
+
+        processTestBoolean(value, getBooleanPresentation(value));
+    }
+
+    @Test
+    public void testBooleanFalse() {
+        BooleanNode value = getBooleanFalse();
+
+        processTestBoolean(value, getBooleanPresentation(value));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        ArrayNode array = getEmptyArray();
+
+        processTestArray(array, getArrayPresentation(array));
+    }
+
+    @Test
+    public void testArray() {
+        ArrayNode array = getArray();
+
+        processTestArray(array, getArrayPresentation(array));
+    }
+
+    @Test
+    public void testEmptyObject() {
+        ObjectNode object = getEmptyObject();
+
+        processTestObject(object, getObjectPresentation(object));
+    }
+
+    @Test
+    public void testObject() {
+        ObjectNode object = getObject();
+
+        processTestObject(object, getObjectPresentation(object));
+    }
+
+    protected abstract void processTestNull(NullNode value, JsonNode presentation);
+
+    protected abstract void processTestNumberInt(NumericNode value, JsonNode presentation);
+
+    protected abstract void processTestNumberLong(NumericNode value, JsonNode presentation);
+
+    protected abstract void processTestNumberDouble(NumericNode value, JsonNode presentation);
+
+    protected abstract void processTestString(TextNode value, JsonNode presentation);
+
+    protected abstract void processTestBoolean(BooleanNode value, JsonNode presentation);
+
+    protected abstract void processTestArray(ArrayNode array, JsonNode presentation);
+
+    protected abstract void processTestObject(ObjectNode object, JsonNode presentation);
 
     protected Optional<JsonNode> getPresentation(JsonNode json) {
         if (json instanceof NullNode) {
