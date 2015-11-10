5e22e08d2192dcfbced799922022865ae836885e
Maks Atygaev <jxcoder@ya.ru>
2015-11-10 18:20:49 +0600

    Implement ArrayStyleDynamicDecoding

---
+++ src/main/java/com/github/nsnjson/decoding/style/ArrayStyleDynamicDecoding.java
@@ -0,0 +1,5 @@
+package com.github.nsnjson.decoding.style;
+
+import com.github.nsnjson.decoding.DynamicDecoding;
+
+public class ArrayStyleDynamicDecoding extends ArrayStyleDecoding implements DynamicDecoding {}
