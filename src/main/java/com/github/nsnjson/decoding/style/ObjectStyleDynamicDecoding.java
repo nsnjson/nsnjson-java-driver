1d66f8d7b309f88fbebbcc2217d949dc59bd30bb
Maks Atygaev <jxcoder@ya.ru>
2015-11-10 18:21:03 +0600

    Implement ObjectStyleDynamicDecoding

---
+++ src/main/java/com/github/nsnjson/decoding/style/ObjectStyleDynamicDecoding.java
@@ -0,0 +1,5 @@
+package com.github.nsnjson.decoding.style;
+
+import com.github.nsnjson.decoding.DynamicDecoding;
+
+public class ObjectStyleDynamicDecoding extends ObjectStyleDecoding implements DynamicDecoding {}
