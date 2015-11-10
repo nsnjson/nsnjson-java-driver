cf4f468c717d3b4b42d18954a81ef502c2f49ac5
Maks Atygaev <jxcoder@ya.ru>
2015-11-10 18:19:57 +0600

    Implement ArrayStyleDynamicEncoding

---
+++ src/main/java/com/github/nsnjson/encoding/style/ArrayStyleDynamicEncoding.java
@@ -0,0 +1,5 @@
+package com.github.nsnjson.encoding.style;
+
+import com.github.nsnjson.encoding.DynamicEncoding;
+
+public abstract class ArrayStyleDynamicEncoding extends ArrayStyleEncoding implements DynamicEncoding {}
